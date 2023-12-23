57ab97aacbc2b17a1ed18b560e8231894a549e7c
BriceNogue <bricenogue@gmail.com>
2023-12-23 16:35:07 +0100

    nav-bar

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,20 @@
 <template>
-    <header>
-      <div class="navbar">
+    <header  ref="header">
+      <div class="nav-bar">
         <div class="logo">
           <a to="/">
             <img src="../assets/LogoAlpha-B.png" alt="">
           </a>
         </div>  
         <nav>
-          <RouterLink class="nav-link" to="/">About me</RouterLink>
-          <RouterLink class="nav-link" to="/projects">Projects</RouterLink>
+          <router-link class="nav-link text-blue-600 active:text-red-600" to="/">About me</router-link>
+          <router-link class="nav-link text-blue-600 active:text-red-600" to="/projects">Education</router-link>
+          <router-link class="nav-link text-blue-600 active:text-red-600" to="/projects">Skills</router-link>
+          <router-link class="nav-link text-blue-600 active:text-red-600" to="/projects">Experiences</router-link>
+          <router-link class="nav-link text-blue-600 active:text-red-600" to="/projects">Projects</router-link>
         </nav>
-        <div class="navBtn">
-          <button>Contact me</button>
+        <div class="nav-btn">
+          <button class="btn-contact bg-blue-600 text-white">Contact me</button>
         </div>
       </div>
     </header>
@@ -28,21 +31,34 @@ export default {
     },
 
     mounted() {
-        
+      window.addEventListener('scroll', this.handleScroll);
+    },
+
+    beforeUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
     },
 
     methods: {
-        
+      handleScroll() {
+        if (window.scrollY > 150) {
+          // Ajoutez la classe 'fixed' à l'élément du header
+          this.$refs.header.classList.add('fixed');
+        } else {
+          // Supprimez la classe 'fixed' de l'élément du header
+          this.$refs.header.classList.remove('fixed');
+        }
+      },
     },
 };
 </script>
 
 <style lang="scss" scoped>
 header {
-  background: black;
   width: 100%;
+  background-color: transparent;
+  z-index: 1;
 }
-.navbar {
+.nav-bar {
   width: 100%;
   height: 80px;
   display: flex;
@@ -66,8 +82,15 @@ nav {
     text-indent: 15px;
   }
 
-  .nav-link:hover {
-    
+  .router-link-exact-active {
+    font-size: 28px;
   }
+}
+
+.btn-contact {
+  width: 150px;
+  height: 40px;
+  border-radius: 25px;
+  font-size: 18px;
 }
 </style>
